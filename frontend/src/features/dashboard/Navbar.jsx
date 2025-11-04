86f60b89ac51cccf263c3e8628b5ae53d2d8ed1a
Osas706 <92319443+Osas706@users.noreply.github.com>
2025-11-04 16:37:23 +0100

    Layout Page & Navbar component

---
+++ frontend/src/features/dashboard/Navbar.jsx
@@ -0,0 +1,44 @@
+import { FileText, LogOut } from "lucide-react";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function Navbar() {
+  const user = { 
+    name: "John Doe", 
+    email: "doe@gmail.com" 
+  };
+
+  const navigate = useNavigate();
+  const logout = async () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="shadow bg-white">
+      <nav className="flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5">
+        <Link
+          to={"/"}
+          className="text-xl md:text-2xl font-semibold flex items-center "
+        >
+          <span className="flex items-center text-indigo-600">
+            <FileText className="w-6 h-6 md:w-8 md:h-8" />
+            Create{" "}
+          </span>
+          Resume
+        </Link>
+
+        <div className="flex items-center gap-4 text-sm">
+          <p className="max-sm:hidden">Hi, {user?.name}</p>
+          <button
+            onClick={logout}
+            className="flex items-center gap-2 bg-white hover:bg-slate-50 border border-gray-300 px-5 py-1.5 rounded-lg font-medium active:scale-95 transition-all hover:shadow-sm"
+          >
+            Logout <LogOut size={15} />
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../features/dashboard/Navbar";
 
 function Layout() {
   return (
-    <div className="">
-      Layout
-
-      <div className="">
+    <div>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
         <Outlet />
       </div>
     </div>
