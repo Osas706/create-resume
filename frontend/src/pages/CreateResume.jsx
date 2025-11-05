c351906c0b397bfddc401acf1cefdaede7f9988e
Osas706 <92319443+Osas706@users.noreply.github.com>
2025-11-05 20:53:08 +0100

    CreateResume Page

--- frontend/src/pages/CreateResume.jsx
+++ frontend/src/pages/CreateResume.jsx
@@ -1,11 +1,137 @@
-import React from 'react'
+import { dummyResumeData } from "@/assets/assets";
+
+import {
+  ArrowLeft,
+  Briefcase,
+  ChevronLeft,
+  ChevronRight,
+  FileText,
+  Folder,
+  GraduationCap,
+  Sparkle,
+  User,
+} from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 function CreateResume() {
+  const { resumeId } = useParams();
+
+  const [resumeData, setResumeData] = useState({
+    _id: "",
+    title: "",
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3b82f6",
+    public: false,
+  });
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [removeBackground, setRemoveBackground] = useState(false);
+
+  // loadExistingResume
+  const loadExistingResume = async () => {
+    const resume = dummyResumeData.find((resume) => resume._id === resumeId);
+    if (resume) {
+      setResumeData(resume);
+      document.title = resume.title;
+    }
+  };
+
+  useEffect(() => {
+    loadExistingResume();
+  }, []);
+
+  const sections = [
+    { id: "personal", name: "Personal Info", icon: User },
+    { id: "summary", name: "Summary", icon: FileText },
+    { id: "experience", name: "Experience", icon: Briefcase },
+    { id: "education", name: "Education", icon: GraduationCap },
+    { id: "projects", name: "Projects", icon: Folder },
+    { id: "skills", name: "Skills", icon: Sparkle },
+  ];
+  const activeSection = sections[activeSectionIndex];
+
   return (
     <div>
-      CreateResume
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <Link
+          to={"/app"}
+          className="inline-flex items-center gap-1 text-slate-500 hover:text-slate-700 transition-all"
+        >
+          <ArrowLeft className="size-4" />
+          Back to Dashboard
+        </Link>
+      </div>
+
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <div className="grid lg:grid-cols-12 gap-8">
+          {/* left panel -- FORM */}
+          <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
+            <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1">
+              {/* progress bar */}
+              <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200 " />
+              <hr
+                className="absolute top-0 left-0 h-1 bg-linear-to-r from-indigo-500 to-indigo-600 border-none transi duration-2000"
+                style={{
+                  width: `${
+                    (activeSectionIndex * 100) / (sections.length - 1)
+                  } %`,
+                }}
+              />
+
+              {/* section navigation */}
+              <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
+                <div className=""></div>
+
+                <div className="flex items-center">
+                  {activeSectionIndex !== 0 && (
+                    <button
+                      onClick={() =>
+                        setActiveSectionIndex((prev) => Math.max(prev - 1, 0))
+                      }
+                      className="flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all"
+                      disabled={activeSectionIndex === 0}
+                    >
+                      <ChevronLeft className="size-4" /> Previous
+                    </button>
+                  )}
+
+                  <button
+                    onClick={() =>
+                      setActiveSectionIndex((prev) =>
+                        Math.min(prev + 1, sections.length - 1)
+                      )
+                    }
+                    className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${
+                      activeSectionIndex === sections.length - 1 && `opacity-50`
+                    }`}
+                    disabled={activeSectionIndex === sections.length - 1}
+                  >
+                    Next <ChevronRight className="size-4" />
+                  </button>
+                </div>
+              </div>
+
+              {/* *** form *** */}
+              <div className="space-y-6">
+                {activeSection.id === "personal" && (
+                  <div className=""></div>
+                )}
+              </div>
+            </div>
+          </div>
+
+          {/* right panel -- PREVIEW */}
+          <div className=""></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateResume
+export default CreateResume;
