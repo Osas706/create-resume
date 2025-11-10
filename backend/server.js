eea8d7e7b41c595fe3078c7afa5ee045fab2b079
Osas706 <92319443+Osas706@users.noreply.github.com>
2025-11-10 10:09:37 +0100

    Updated Server with AuthRoute

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectToDB from './db/db.js';
+import authRoutes from "./routes/AuthRoute.js";
 
 // database connection
 await connectToDB();
@@ -12,6 +13,9 @@ const PORT = process.env.PORT || 3000;
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+// routes
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`✅ Server is running on http://localhost:${PORT}`);
