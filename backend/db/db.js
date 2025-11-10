8d5f716dd2c92bdaa6163ebffa4315dd6bf7a83e
Osas706 <92319443+Osas706@users.noreply.github.com>
2025-11-10 07:17:07 +0100

    Backend setup and DB connection

---
+++ backend/db/db.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const connectToDB = async () => {
+  try {
+    let mongodbURL = process.env.MONGODB_URL;
+    const projectName = "create-resume";
+
+    if (!mongodbURL) {
+      throw new Error("MONGODB_URL environment variable not set");
+    }
+
+    if (mongodbURL.endsWith("/")) {
+      mongodbURL = mongodbURL.slice(0, -1);
+    }
+
+    const fullURL = `${mongodbURL}/${projectName}`;
+
+    await mongoose.connect(fullURL);
+
+    console.log("✅ Database connected successfully");
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error);
+    // process.exit(1); // stop the app if DB fails
+  }
+};
+
+export default connectToDB;
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectToDB from './db/db.js';
+
+// database connection
+await connectToDB();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+})
