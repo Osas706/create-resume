c35fe36ee4be980fd8859084d4bc956213633a1e
Osas706 <92319443+Osas706@users.noreply.github.com>
2025-11-12 10:31:16 +0100

    getUserData func

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,71 @@
-import {Routes, Route, Navigate} from 'react-router-dom';
-import Home from './pages/Home';
-import Layout from './pages/Layout';
-import Dashboard from './pages/Dashboard';
-import CreateResume from './pages/CreateResume';
-import Preview from './pages/Preview';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import CreateResume from "./pages/CreateResume";
+import Preview from "./pages/Preview";
 
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import ForgotPassword from './pages/auth/ForgotPassword';
-import ResetPassword from './pages/auth/ResetPassword';
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword";
+import { useDispatch } from "react-redux";
+import api from "./config/api";
+import { login, setLoading } from "./store/features/authSlice";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+
+  // getUserData
+  const getUserData = async () => {
+    const token = localStorage.getItem("token");
+
+    try {
+      if (token) {
+        const { data } = await api.get("/api/user/data", {
+          headers: { Authorization: token },
+        });
+        if (data.user) {
+          dispatch(login({ token, user: data.user }));
+        }
+
+        dispatch(setLoading(false));
+      }
+    } catch (error) {
+      console.log(error, "Error in getUserData func");
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+
+  useEffect(() => {
+    getUserData()
+  }, [])
+  
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/view/:resumeId' element={<Preview />} /> 
+        <Route path="/" element={<Home />} />
+        <Route path="/view/:resumeId" element={<Preview />} />
 
-        <Route path='/app' element={<Layout />} >
+        <Route path="/app" element={<Layout />}>
           <Route index element={<Dashboard />} />
-          <Route path='/app/create-resume/:resumeId' element={<CreateResume />} />
+          <Route
+            path="/app/create-resume/:resumeId"
+            element={<CreateResume />}
+          />
         </Route>
 
-        <Route path='/auth/login' element={<Login />} />
-        <Route path='/auth/register' element={<Register />} />
-        <Route path='/auth/forgot-password' element={<ForgotPassword />} />
-        <Route path='/auth/reset-password' element={<ResetPassword />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/forgot-password" element={<ForgotPassword />} />
+        <Route path="/auth/reset-password" element={<ResetPassword />} />
 
-        <Route path='*' element={<Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
